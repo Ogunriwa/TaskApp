b6c8a6b7376f4b6d8c052145109b069ea2788f31
Ogunriwa <ibrahimaro4@gmail.com>
2025-01-09 18:29:42 -0600

    cgc

--- Sources/App/Migrations/CreateUserToken.swift
+++ Sources/App/Migrations/CreateUserToken.swift
@@ -10,22 +10,16 @@ import Fluent
 
 
 struct CreateUserToken: AsyncMigration {
-    
-    var name: String { "CreateUserToken" }
-    
     func prepare(on database: Database) async throws {
-        
         try await database.schema("user_tokens")
             .field("id", .int64, .identifier(auto: true))
             .field("token", .string, .required)
-            .field("user_id", .uuid, .required, .references("users", "id"))
+            .field("user_id", .int64, .required, .references("users", "id"))
             .unique(on: "token")
             .create()
-            
     }
-    
+
     func revert(on database: Database) async throws {
-        
         try await database.schema("user_tokens").delete()
     }
 }
